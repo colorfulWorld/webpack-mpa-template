4a03320d71beab8e00d0cd5531efa72214a77f60
colorfulworld <colorfulworld.yu@gmail.com>
2021-05-27 17:57:58 +0800

    feat:小调整

--- config/webpack.dev.conf.js
+++ config/webpack.dev.conf.js
@@ -1,38 +1,38 @@
-const path = require("path");
-const webpack = require("webpack");
-const merge = require("webpack-merge");
-const webpackConfigBase = require("./webpack.base.conf");
+const path = require('path')
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const webpackConfigBase = require('./webpack.base.conf')
 const webpackConfigDev = {
-  mode: "development", // 通过 mode 声明开发环境
+  mode: 'development', // 通过 mode 声明开发环境
   output: {
-    path: path.resolve(__dirname, "../dist"),
+    path: path.resolve(__dirname, '../dist'),
     // 打包多出口文件
     // 生成 a.bundle.js  b.bundle.js
-    filename: "./js/[name].bundle.js",
+    filename: './js/[name].bundle.js'
   },
   devServer: {
-    contentBase: path.join(__dirname, "../src"), //本地服务器所加载文件的目录
+    contentBase: path.join(__dirname, '../src'), //本地服务器所加载文件的目录
     watchContentBase: true,
-    publicPath: "/",
-    host: "127.0.0.1",
-    port: "8089",
+    publicPath: '/',
+    host: '127.0.0.1',
+    port: '8089',
     overlay: true, // 浏览器页面上显示错误
     open: true, // 开启浏览器
-    openPage: "demo1.html",
+    openPage: 'demo1.html',
     // stats: "errors-only", //stats: "errors-only"表示只打印错误：
-    hot: true, // 开启热更新
+    hot: true // 开启热更新
     // hotOnly:true //hot 和 hotOnly 的区别是在某些模块不支持热更新的情况下，前者会自动刷新页面，后者不会刷新页面，而是在控制台输出热更新失败
   },
   plugins: [
     //热更新
-    new webpack.HotModuleReplacementPlugin(),
+    new webpack.HotModuleReplacementPlugin()
   ],
   optimization: {
-    minimize: false,
+    minimize: false
   },
-  devtool: "source-map",
+  devtool: 'source-map', //若是不开启则无法定位开发时的错误代码
   module: {
-    rules: [],
-  },
-};
-module.exports = merge(webpackConfigBase, webpackConfigDev);
+    rules: []
+  }
+}
+module.exports = merge(webpackConfigBase, webpackConfigDev)
--- src/pages/demo1/index.js
+++ src/pages/demo1/index.js
@@ -1,19 +1,19 @@
 //基础配置
-import "lib-flexible";
+import 'lib-flexible'
 
 //业务配置
-import "../../css/common.css";
-import "../../css/test.css";
-import "./index.less";
-import {title} from "../../js/common";
-
-window.onload = function(){
-  console.log(1);
-  $(".div1").html("一生二，二生三，三生万物!!");
-  $(".div2").html(title);
-  console.log("process.env.NODE_ENV:",process.env.NODE_ENV);
-  // { 
+import '../../css/common.css'
+import '../../css/test.css'
+import './index.less'
+import { title } from '../../js/common'
+console.log(r.e)
+window.onload = function () {
+  console.log(1)
+  $('.div1').html('一生二，二生三，三生万物!!')
+  $('.div2').html(title)
+  console.log('process.env.NODE_ENV:', process.env.NODE_ENV)
+  // {
   //     demo1: './src/pages/demo1/index.js',
-  //     demo2: './src/pages/demo2/index.js'   
+  //     demo2: './src/pages/demo2/index.js'
   // }
-};
+}
